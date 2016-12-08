c6d828e4414358e4d8e3ee961f11a19eb449411f
kuriza27 <cj.bulacito@gmail.com>
2016-12-08 19:47:08 +0800

    Update
    css for school po

--- resources/views/schoolpo.blade.php
+++ resources/views/schoolpo.blade.php
@@ -36,15 +36,13 @@
 @section('content')
 <div class="container school_po_content">
 	<p>
-	<b>Step 1:</b>Fill up and select the appropriate option of your order details. Once done entering all the details, click Submit My PO button below.<br>
+	<b>Step 1:</b> Fill up and select the appropriate option of your order details. Once done entering all the details, click Submit My PO button below.<br>
 
 	<b>Step 2:</b> You will received an email confirmation that we already received your PO and will be under review.<br>
 
-
 	<b>Step 3:</b> Reply to that same email with your shipping and billing address. Also attach a signed W9 form.<br>
 
-	<b>Step 4: </b>A representative will reply back with a sample design for your approval and confirm the order details. Credit Cards, Check, and Paypal are the available methods of payment.<br>
-<br>
+	<b>Step 4:</b> A representative will reply back with a sample design for your approval and confirm the order details. Credit Cards, Check, and Paypal are the available methods of payment.<br>
 	<b>Additional Information:</b><br>
 
 	<b>1.</b> Terms: NET 15<br>
